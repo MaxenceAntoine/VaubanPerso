25100d7831e0f6bf626ca2d3198a591262ad4921
Maxence Antoine <manxence.antoine08@icloud.com>
2023-09-28 09:46:09 +0200

    Ajout de l'attente du chargement du dom en jquery

--- La Lettre Palm Beach/test.js
+++ La Lettre Palm Beach/test.js
@@ -1,1 +1,5 @@
-alert("test.js");
+ // Attendre que le DOM soit prêt
+ $(document).ready(function () {
+    // Afficher une alerte lorsque la page est chargée
+    alert('La page est chargée !');
+});
